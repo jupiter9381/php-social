a989702a873b28ae50e8c10207e541dcb5ffa961
jupiter9381 <jupiter9381@gmail.com>
2019-09-16 03:13:06 +1000

    index page added

---
+++ index.php
@@ -0,0 +1,352 @@
+<!--header-->
+<?php include("include/header.php");?>
+<?php include("include/top_navigation.php");?>
+<!--header-end-->
+<title>Buy Real Social Media Followers, likes & Views - FollowMyAss</title>
+<meta name="description" content="FollowMyAss offers Real and Active followers, likes, and views on YouTube, Instagram, and Discord. We have advanced security that restricts bot access & preserves the integrity of your follower base. FollowMyAss connects people purchasing followers with people who will follow them for money.">
+<meta name="keywords" content="buy followers on instagram free,buy followers and likes,buy followers cheap,buy followers on instagram app,buy followers on instagram india,buy followers app store,buy followers active,buy instagram followers app,buy insta followers app,buy instagram followers and likes,buy instagram followers best site,buy instagram followers cheap,buy instagram followers cheap 10k,buy cheapest instagram followers">
+<link rel="canonical" href="https://followmyass.com/" />
+<meta name="google-site-verification" content="Z-sDn8XIryY-7QVO_8A0BgMoxKCAg2wO-DqeDtnQSog" />
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-145665348-1"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-145665348-1');
+</script>
+
+</head>
+
+<div class="row home homepage-id container">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		
+		<div class="row home-row-1">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 buy-or-like">
+				
+				<img class="img-responsive cart-full-img" src="img/cart-full.png" />
+				<h3>Buy Followers or 
+				Likes</h3>
+				<a class="home-offer-btn" href="<?php echo BASE_URL ?>/buy-followers">Make An Offer</a>
+			</div>
+			
+			
+			
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 make-money-following">
+				<img class="img-responsive cart-full-img" src="img/dollar-group.png" />
+				<h3>Make Money
+				Following People</h3>
+				<a class="home-offer-btn" href="<?php echo BASE_URL ?>/follow-for-money">Browse Offers</a>
+			</div>
+			
+		</div>
+
+		
+		
+		<div class="row home-row-2">
+
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+
+				<div class="row row-inner">
+
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<h1>Beneﬁts For Everyone</h1>
+
+						<p>Promote Your Brand, Gain Publicity Or Just Make A Quick Buck.
+						Everyone Stands To Win.</p>
+						<img class="img-responsive benefits-img" src="img/benefits-logo.png" />
+						<h1>100% Real Followers and Likes</h1>
+
+						<p>100% Real Followers and Real Likes (No bots). You get real 
+							followers from our marketplace with advanced security 
+							that restricts bot access & preserves the integrity of your 
+						follower base.</p>
+					</div>
+				</div>
+
+
+
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+	</div>
+	<!-------End of Body container--------->
+</div>
+
+
+
+<div class="row home-row-3">
+	<div class=" homepage-id container">
+		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+			<img class="img-responsive aim-board-img" src="img/aim-arrow.png" />
+
+		</div>
+
+		<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+			<h3>Why Choose  FollowMyAss.com?</h3>
+			<p>We provide real followers, have great security, and we 
+				give you control over your orders. Other websites don’t 
+				disclose where followers come from and 
+				likely use bots. Each purchase on followmyass.com has a 
+				unique tracking page. You can access these pages to view 
+				progress on each campaign, including number of 
+			followers, identity of followers, and percent completed. </p>
+			<center><a class="home-why-choose-btn" href="https://followmyass.com/buy-followers">Make An Offer</a>
+			</center>
+		</div>
+	</div>
+</div>
+
+
+<div class="row home-row-4 zai">
+
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class=" homepage-id container">
+
+
+
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Left and right controls -->
+
+
+				<div class="carousel-navigations">     
+					<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left">
+							<img class="img-responsive arrow-left" src="img/arrow-left.png" />
+
+						</span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right">
+							<img class="img-responsive arrow-left right-arrow" src="img/arrow-right.png" />
+						</span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+
+				</ol>
+
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner">
+					<div class="item active">
+						<div class="row">
+							<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+								<div class="review-author">
+									<img class="img-responsive review-dp" src="img/nathan.png" />
+									
+
+								</div>
+								<div class="review-content">
+									<h4 class="ra-author">Nathan Hughes </h4>
+
+									<img src="img/ewview-stars.png" />
+									<p>  <img class="before-quite-icon" src="img/quote-before.png" />Highly recommend. Its safe, and cheap... Great service!<img class="after-quite-icon" src="img/quote-after.png" /></p>
+								</div>
+								
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+								<div class="review-author">
+									<img class="img-responsive review-dp" src="img/aaron.png" />
+
+									
+
+								</div>
+								<div class="review-content">
+									<h4 class="ra-author">Aaron West</h4>
+									<img class="before-quite-icon" src="img/ewview-stars.png" />
+									<p><img src="img/quote-before.png" />It worked for me! Finally purchased after doing a ton of research. <img class="after-quite-icon" src="img/quote-after.png" /> </p>
+								</div>
+							</div>
+							<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+								<div class="review-author">
+									<img class="img-responsive review-dp" src="img/Frances.png" />                                      
+
+
+								</div>
+								<div class="review-content">
+
+									<h4 class="ra-author">Frances Richardson</h4>
+									<img src="img/ewview-stars.png" />
+									<p><img class="before-quite-icon" src="img/quote-before.png" />I'm obsessed with this website and their quality. 5/5! <img class="after-quite-icon" src="img/quote-after.png" /> </p>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+
+
+
+
+					<div class="item second-item">
+						<div class="row">
+							<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+								<div class="review-author">
+									<img class="img-responsive review-dp" src="img/Mahmud.jpg" />
+									
+
+								</div>
+								<div class="review-content">
+									<h4 class="ra-author">Mahmud Alford</h4>
+									<img src="img/ewview-stars.png" />                                                            <p><img class="before-quite-icon" src="img/quote-before.png" />100% 
+										Recommend. It’s 
+										safe and 
+										cheap...great 
+										service!<img class="after-quite-icon" src="img/quote-after.png" /></p>
+									</div>
+									
+								</div>
+								<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+									<div class="review-author">
+										<img class="img-responsive review-dp" src="img/lexu.jpg" />
+
+										
+
+									</div>
+									<div class="review-content">
+										<h4 class="ra-author">Lexi Westbrook</h4>
+										<img src="img/ewview-stars.png" />
+										<p><img class="before-quite-icon" src="img/quote-before.png" />The transparency and control over campaigns makes this by far the best service.  <img class="after-quite-icon" src="img/quote-after.png" /></p>
+									</div>
+								</div>
+								<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+									<div class="review-author">
+										<img class="img-responsive review-dp" src="img/will-2.jpg" />                                      
+
+
+									</div>
+									<div class="review-content">
+
+										<h4 class="ra-author">Will Cruz</h4>
+										<img src="img/ewview-stars.png" />
+										<p><img class="before-quite-icon" src="img/quote-before.png" />Made me 10 times 
+											more popular. 
+											Great service!<img class="after-quite-icon" src="img/quote-after.png" /></p>
+										</div>
+										
+									</div>
+								</div>
+							</div>
+							
+
+
+
+							<div class="item second-item">
+								<div class="row">
+									<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+										<div class="review-author">
+											<img class="img-responsive review-dp" src="img/Leah.png" />
+											
+
+										</div>
+										<div class="review-content">
+											<h4 class="ra-author">Leah Reyes</h4>
+
+											<img src="img/ewview-stars.png" />
+											<p>  <img class="before-quite-icon" src="img/quote-before.png" />Paid 20$ for likes. Took a while, but they were high quality. 
+												<img class="after-quite-icon" src="img/quote-after.png" /></p>
+											</div>
+											
+										</div>
+										<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+											<div class="review-author">
+												<img class="img-responsive review-dp" src="img/elle.png" />
+
+												
+
+											</div>
+											<div class="review-content">
+												<h4 class="ra-author">Elle Rose</h4>
+												<img class="before-quite-icon" src="img/ewview-stars.png" />
+												<p><img src="img/quote-before.png" />The transparency and control over campaigns makes this by far the best service. 
+													<img class="after-quite-icon" src="img/quote-after.png" /> </p>
+												</div>
+											</div>
+											<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+												<div class="review-author">
+													<img class="img-responsive review-dp" src="img/will.png" />                                      
+
+
+												</div>
+												<div class="review-content">
+
+													<h4 class="ra-author">Will Cruz</h4>
+													<img src="img/ewview-stars.png" />
+													<p><img class="before-quite-icon" src="img/quote-before.png" />Made me 10 times more popular. Great service!<img class="after-quite-icon" src="img/quote-after.png" /> </p>
+												</div>
+												
+											</div>
+										</div>
+									</div>
+
+
+
+								</div>
+							</div>
+
+
+						</div>
+
+
+
+
+
+
+
+
+
+
+
+					</div>
+				</div>
+
+
+
+
+
+
+				<?php include("include/footer.php");?>
+
+				<!-- /footer-->
+
+
+				<script>
+
+					$(document).ready(function(){
+						$("#myCarousel").carousel({
+
+							interval: 5000
+
+
+						});
+
+						$(".buy-or-like").click(function(){
+							window.location.href = '<?php echo BASE_URL ?>/buy-followers';
+						});     
+
+						$(".make-money-following").click(function(){
+							window.location.href = '<?php echo BASE_URL ?>/follow-for-money';
+						});
+						
+					});
+
+					
+				</script>
+
+			</body>
+
+			</html>
