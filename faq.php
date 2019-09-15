7e6198179b2be2f49e04f49366eaa01d33425ce3
jupiter9381 <jupiter9381@gmail.com>
2019-09-16 04:53:22 +1000

    faq page

---
+++ faq.php
@@ -0,0 +1,360 @@
+<!--header-->
+<?php include 'include/header.php';?>
+<?php include("include/top_navigation.php");?>
+<!--header-end-->
+<title>Frequently Asked question - Buying Followers | followmyass.com</title>
+<meta name="description" content="Look at frequently asked questions for buying real YouTube, Instagram, Facebook, and Discord Followers, Likes, and Views.">
+<meta name="keywords" content="buy followers on instagram free,buy followers and likes,buy followers cheap,buy followers on instagram app,buy followers on instagram india,buy followers app store,buy followers active,buy instagram followers app,buy insta followers app,buy instagram followers and likes,buy instagram followers best site,buy instagram followers cheap,buy instagram followers cheap 10k,buy cheapest instagram followers">
+</head>
+
+<style>
+    
+.accordianheader {color:#000; background:#fff; padding:15px 35px;  text-align: left;}
+.accordianbody {background:#f4f4f4;}
+.accordianbody ul {margin:0; list-style:none; padding:0;}
+.accordianbody ul li {padding:10px; border-bottom: 1px solid lightgrey;}
+
+i { position:absolute;
+  right: 20px;
+  top:14px;
+    -webkit-transition:all 300ms ease-in 0s;
+    -moz-transition: all 300ms ease-in 0s;
+    -o-transition: all 300ms ease-in 0s;
+  transition: all 300ms ease-in 0s;}
+
+.ui-state-active i {
+    color: #ACD4CE;
+    -webkit-transform: rotate(180deg);
+    -moz-transform: rotate(180deg);
+    -o-transform: rotate(180deg);
+    -ms-transform: rotate(180deg);
+    transform: rotate(180deg);
+}
+.ui-accordion .ui-accordion-header .ui-accordion-header-icon {
+    display: none;
+}
+.accordianheader h3 {
+    font-size: 1.17em;
+        font-family: 'Lato-Medium', sans-serif;
+    margin: 0 auto;
+}
+
+.ui-accordion .ui-accordion-icons {
+    padding-left: 25px !important;
+    padding-top: 20px !important;
+    padding-bottom: 30px !important;
+    padding-right: 15px !important;
+    border: none;
+    -webkit-box-shadow: 0px 0px 10px 0px #dedddd;
+    box-shadow: 0px 0px 10px 0px #dedddd;
+    margin: 10px 0px 0px 0px;
+}
+.ui-accordion-header-active {
+    background: #2273d5;
+        color: #fff;
+
+}
+.ui-accordion .ui-accordion-icons {
+    padding-left: 35px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+        padding-right: 15px;
+
+    border: none;
+    -webkit-box-shadow: 0px 0px 10px 0px #dedddd;
+    box-shadow: 0px 0px 10px 0px #dedddd;
+        margin: 10px 0px 0px 0px;
+
+}
+.accordianbody {
+    background: #e8f0fe;
+    border: none;
+}
+.ui-state-active img.img-angle-down {
+    -webkit-transform: rotate(180deg);
+    -moz-transform: rotate(180deg);
+    -o-transform: rotate(180deg);
+    -ms-transform: rotate(180deg);
+    transform: rotate(180deg);
+}
+.accor-head-wrap {
+    float: left;
+    width: 96%;
+}
+.ui-accordion .ui-accordion-content {
+    padding: 1.9em 2.2em;
+    border-top: 0;
+    overflow: auto;
+}
+
+@media only screen and (max-width: 1120px) {
+    
+.faq-inimation-wrap {
+    display:none;
+}
+
+}
+
+@media only screen and (min-width: 820px) {
+    
+div#accordion {
+    max-width: 60%;
+    margin: 0 auto 70px;
+}
+}
+@media only screen and (max-width: 600px) {
+.accordianheader h3 {
+    font-size: 13px;
+    margin: 0 auto;
+    font-family: 'Lato-Medium', sans-serif;
+    font-weight: 500;
+}
+.accor-head-wrap {
+    float: left;
+    width: 96%;
+}
+img.img-angle-down {
+    max-width: 15px;
+
+}
+}
+h2.faq-page-title {
+    text-align: center;
+    margin-bottom: 55px;
+}
+img.img-angle-down {
+    position: relative;
+    top: 6px;
+}
+.faq-inimation-wrap {
+    position: absolute;
+    right: 0px;
+    top: 46%;
+}.faq-inimation-wrap img {
+    max-width: 240px;
+}
+.accor-head-wrap a {
+    color: #000;
+}
+.ui-accordion-header-active .accor-head-wrap a{
+    color:#fff;
+    
+}
+@media only screen and (min-width: 1600px) {
+.faq-inimation-wrap img {
+    max-width: none;
+}
+
+.faq-inimation-wrap {
+    position: absolute;
+    right: 0px;
+    top: 38%;
+}
+}
+@media only screen and (min-width: 1800px) {
+    .container.full-height.faq {
+    min-height: 812px;
+}
+.faq-inimation-wrap img {
+    max-width: none;
+}
+
+.faq-inimation-wrap {
+    position: absolute;
+    right: 0px;
+    top: 28% !important;
+}
+}
+@media only screen and (max-width: 450px) {
+    div#accordion {
+    margin-bottom: 70px;
+}
+.accor-head-wrap {
+    float: left;
+    width: 93%;
+}
+}
+
+
+</style>
+
+<!--header-end-->
+<div class="container full-height faq">
+
+<h1 class="faq-page-title">How can we help you?</h1>
+
+
+<div id="accordion" class="ui-accordion ui-widget ui-helper-reset" role="tablist">
+
+
+<div class="accordianheader ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-5" aria-controls="ui-id-6" aria-selected="false" aria-expanded="false" tabindex="-1">
+
+<div class="accor-head-wrap">
+<a href="JavaScript:void(0);">
+<h3>
+Are the followers real?</h3></a>
+</div>
+<img class="img-angle-down" src="/img/arrow-grey.png"></img>
+</div>
+<div class="accordianbody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-6" aria-labelledby="ui-id-5" role="tabpanel" aria-hidden="true" style="display: none;">
+<p>
+You get real followers from our marketplace with advanced security restricting bot access and preserving the integrity of your follower base. 
+
+ </p>
+</div>
+
+
+
+
+
+
+
+<div class="accordianheader ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-5" aria-controls="ui-id-6" aria-selected="false" aria-expanded="false" tabindex="-1">
+<div class="accor-head-wrap">
+<a href="JavaScript:void(0);">
+<h3>
+Why should I buy followers?</h3>
+</a>
+</div>
+<img class="img-angle-down" src="/img/arrow-grey.png"></img>
+</div>
+<div class="accordianbody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-6" aria-labelledby="ui-id-5" role="tabpanel" aria-hidden="true" style="display: none;">
+<p>
+If you own a business, buying followers will mark your service/product as a popular entity. When people see a lot of followers for a company, they think the companies more legitimate. The same thing for people, if you have more followers, people will think your more popular. If you want to be perceived as popular, buying followers is a perfect way of achieving that.
+
+
+ </p>
+</div>
+
+
+<div class="accordianheader ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-5" aria-controls="ui-id-6" aria-selected="false" aria-expanded="false" tabindex="-1">
+<div class="accor-head-wrap">
+<a href="JavaScript:void(0);">
+<h3>
+How do I make sure I received all my followers? </h3></a>
+</div>
+<img class="img-angle-down" src="/img/arrow-grey.png"></img>
+</div>
+<div class="accordianbody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-6" aria-labelledby="ui-id-5" role="tabpanel" aria-hidden="true" style="display: none;">
+<p>
+After purchasing the followers, you will redirect to a tracking page. You can see how many followers have yet to follow you, what users followed you, order progress, and other information. 
+
+
+ </p>
+</div>
+
+
+<div class="accordianheader ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-5" aria-controls="ui-id-6" aria-selected="false" aria-expanded="false" tabindex="-1">
+<div class="accor-head-wrap">
+<a href="JavaScript:void(0);">
+<h3>
+Do I need to give you my Password?</h3></a>
+</div>
+<img class="img-angle-down" src="/img/arrow-grey.png"></img>
+</div>
+<div class="accordianbody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-6" aria-labelledby="ui-id-5" role="tabpanel" aria-hidden="true" style="display: none;">
+<p>NEVER, we won't ever ask for your password. We connect your account without needing personal information. 
+
+
+ </p>
+</div>
+
+
+<div class="accordianheader ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-5" aria-controls="ui-id-6" aria-selected="false" aria-expanded="false" tabindex="-1">
+<div class="accor-head-wrap">
+<a href="JavaScript:void(0);">
+<h3>
+Will my followers look like bots (no profile picture, weird random names)?</h3></a>
+</div>
+<img class="img-angle-down" src="/img/arrow-grey.png"></img>
+</div>
+<div class="accordianbody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-6" aria-labelledby="ui-id-5" role="tabpanel" aria-hidden="true" style="display: none;">
+<p>We work hard to keep anyone with no profile picture, or other bot looking accounts away from our website. We stop users without profile pictures from registering. We also allow customers to report followers for added security. We look at these reports and ban users who look like bots. Because of our security, your profile will not show any signs of purchasing followers.
+
+ </p>
+</div>
+
+
+<div class="accordianheader ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-5" aria-controls="ui-id-6" aria-selected="false" aria-expanded="false" tabindex="-1">
+<div class="accor-head-wrap"><a href="JavaScript:void(0);">
+<h3>
+ I no longer want the followers purchased, can they be removed? 
+</h3></a>
+</div>
+<img class="img-angle-down" src="/img/arrow-grey.png"></img>
+</div>
+<div class="accordianbody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-6" aria-labelledby="ui-id-5" role="tabpanel" aria-hidden="true" style="display: none;">
+<p>All followers delivered are real from official Instagram accounts. Due to this benefit, our team cannot transfer or remove followers once they are given. 
+As the Instagram account owner, you may remove selected followers from your follower list by blocking each user.
+
+
+ </p>
+</div>
+
+
+<div class="accordianheader ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-5" aria-controls="ui-id-6" aria-selected="false" aria-expanded="false" tabindex="-1">
+<div class="accor-head-wrap"><a href="JavaScript:void(0);">
+<h3>
+Is buying followers or likes legal? </h3></a>
+</div>
+<img class="img-angle-down" src="/img/arrow-grey.png"></img>
+</div>
+<div class="accordianbody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-6" aria-labelledby="ui-id-5" role="tabpanel" aria-hidden="true" style="display: none;">
+<p>Buying followers for any social media type is legal. It's a digital marketing strategy. 
+
+ </p>
+</div>
+
+
+<div class="accordianheader ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-5" aria-controls="ui-id-6" aria-selected="false" aria-expanded="false" tabindex="-1">
+<div class="accor-head-wrap"><a href="JavaScript:void(0);">
+<h3>
+How can I pay?</h3></a>
+</div>
+<img class="img-angle-down" src="/img/arrow-grey.png"></img>
+</div>
+<div class="accordianbody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-6" aria-labelledby="ui-id-5" role="tabpanel" aria-hidden="true" style="display: none;">
+<p>Our website is designed to accept multiple payment methods. We accept PayPal, Debit/Credit Cards, Stripe, and BitCoin. 
+
+ </p>
+</div>
+
+
+
+
+</div>
+</div>
+
+<div class="faq-inimation-wrap">
+<img src="/img/faq-animation.png" />
+</div>
+<!-- /.container-->
+<!-- footer-->
+<?php include 'include/footer.php';?>
+
+<!-- /footer-->
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+<script id="rendered-js">
+      $( "#accordion" ).accordion({
+      heightStyle: "content",
+      active:false,
+      collapsible: true,
+      header:"div.accordianheader"
+    })
+    
+    
+
+    $('.ui-accordion-header').click(function(){
+   $('img.img-angle-down').attr('src', '/img/arrow-grey.png');
+    if( $('.ui-accordion-header').hasClass('ui-accordion-header-active') ){
+    $('.ui-accordion-header-active img.img-angle-down').attr('src', '/img/arrow-white.png');
+    }
+
+    });
+    
+    
+
+    </script>
+</body>
+
+</html>
