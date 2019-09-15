a1f6eb9f39fba418aa328cb0e4b53c22bc5b1b4d
jupiter9381 <jupiter9381@gmail.com>
2019-09-16 03:36:42 +1000

    about page

---
+++ about-us.php
@@ -0,0 +1,111 @@
+<!--header-->
+<?php include 'include/header.php';?>
+<?php include("include/top_navigation.php");?>
+<!--header-end-->
+<title>About us | How To Buy Followers</title>
+<meta name="description" content="We help people grow their networks and increase visibility while providing valuable employment for active followers.">
+<meta name="keywords" content="about followmyass.com,about us">
+<link rel="canonical" href="https://followmyass.com/about-us" /> 
+</head>
+
+<style>
+    
+    header {
+    margin-bottom: 0px !important;
+}
+</style>
+
+
+
+<div class="about-us">
+    
+<div class="row row-id-1 head-banner">
+
+    
+        <div class="col-lg-12 col-md-12 col-md-12 col-xs-12 head-banner-col">
+        <div class="container">
+
+        <h1>- About Us</h1>
+        </div>
+        </div>
+</div>
+</div>
+
+
+<div class="container full-height about-us-page-id">
+
+<div class="row row-id-2 about-section-1">
+     <div class="col-lg-12 col-md-12 col-md-12 col-xs-12 about-banner-col-1">
+         <img class="img-responsive top-ntch-about" src="/img/about-us-top-notch.png" />
+         <h3>We help people grow their networks and increase visibility while providing valuable employment for active followers.</h3>
+<p>We created FollowMyAss.com to fix two problems that we experienced ourselves. One was the extensive use of bots and fake accounts by sites that sell followers. The second was a lack of meaningful compensation for users willing to help influencers build their networks. 
+We tried websites that promised money for answering questions and none of
+them worked. As we started campaigns we found it difficult to find reliable ways to quickly build our audience. We realized that the intersection of these two problems would create a perfect
+marketplace. That’s how <a href="https://followmyass.com" >followmyass.com </a>was created.</p>
+         </div>
+         </div>
+
+
+
+
+
+<div class="row row-id-3 about-section-2">
+     <div class="col-lg-5 col-md-5 col-md-5 col-xs-5 about-banner-col-left">
+         <img class="img-responsive about-left-sec" src="/img/about-banner-left-sec.png" />
+         </div>
+              <div class="col-lg-7 col-md-7 col-md-7 col-xs-7 about-banner-col-right">
+         <h2>Buy followers or likes</h2>
+         <a class="about-sec-btns" href="https://followmyass.com/buy-followers">Make An Offer</a>
+         </div>
+         </div>
+
+
+
+<div class="row row-id-3 about-section-2 about-sec-3">
+     <div class="col-lg-5 col-md-5 col-md-5 col-xs-5 about-banner-col-left">
+         <img class="img-responsive about-left-sec" src="/img/about-usleft-2new.png" />
+         </div>
+              <div class="col-lg-7 col-md-7 col-md-7 col-xs-7 about-banner-col-right">
+         <h2>Make money following people</h2>
+         <a class="about-sec-btns" href="https://followmyass.com/follow-for-money">Browse Offer</a>
+         </div>
+         </div>
+
+
+<div class="row row-id-4 about-section-3">
+
+              <div class="col-lg-7 col-md-7 col-md-7 col-xs-12 col-xs-12 about-us-col-bot-right">
+         
+         </div>
+         </div>
+
+
+
+
+
+
+</div>
+
+<!------/row End------->
+</div>
+
+<!-- footer-->
+<script>
+		$(document).ready(function(){
+			
+						$(".about-us-page-id .about-section-2").click(function(){
+						  window.location.href = '/buy-followers';
+							});     
+						$(".about-us-page-id .about-section-2.about-sec-3").click(function(){
+						  window.location.href = '/follow-for-money';
+							});
+					
+					});
+
+</script>
+
+<?php include 'include/footer.php';?>
+
+<!-- /footer-->  
+</body>
+</html>
