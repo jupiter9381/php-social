98e161ddac12d39f530126736104e81f3a195121
jupiter9381 <jupiter9381@gmail.com>
2019-10-09 05:12:03 +1000

    follow twitter

---
+++ follow-twitter.php
@@ -0,0 +1,150 @@
+<!--header-->
+<?php include 'include/header.php';?>
+<?php include("include/top_navigation.php");?>
+<!--header-end-->
+
+<div class="container">
+   <div class="row">
+            <div class="col-md-3">              
+            </div>
+            <div class="col-md-6 text-center pd-b-50">
+              <h2><b>Follow Twitter For Money</b></h2>
+            </div>
+
+            <div class="col-md-3">             
+            </div>
+     </div>
+     <div class="follow-container">
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+            <div class="follow-box-col">
+                <div class="follow-for-money-box">
+                  <img src="img/twitter.png">                    
+                    <h3 class="buyer-name">Henry</h3>  
+                    <p class="buyer-price">$0.1 per follow</p>  
+                    <a class="follow-button" href=#>Follow</a>
+                </div>
+            </div>
+        </div>
+     </div>
+     <?php include("include/footer.php");?>
+     </body>
+     </html>
